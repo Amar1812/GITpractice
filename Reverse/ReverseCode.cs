e4866485147e56bf10bd98ad3535addc1fa8629d
Amar Hosamath <amarmh1812@gmail.com>
2023-08-27 15:56:22 +0530

    initial commit

---
+++ Reverse/ReverseCode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");
+            string input = Console.ReadLine();
+
+            Console.WriteLine("The reversed string is: ");
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                Console.Write(input[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
